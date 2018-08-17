020836d6285206102593766e2a9686342c4eb683
pos <pos@CTI-IFSPs-Mac-mini-8.local>
2018-08-17 19:59:03 -0300

    Commit Inicial

---
+++ AgendaXamarin/PagePessoa.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace AgendaXamarin
+{
+    public partial class PagePessoa : ContentPage
+    {
+        public PagePessoa()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ AgendaXamarin/Pessoa.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace AgendaXamarin
+{
+    public class Pessoa
+    {
+        public Pessoa()
+        {
+        }
+    }
+}
